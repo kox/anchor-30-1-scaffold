8a46574240d499e7628f362a9f8f7405842b3b85
kox <garsanzi@gmail.com>
2024-07-09 01:14:38 +0200

    create-token working

--- tests/spl-bug.ts
+++ tests/spl-bug.ts
@@ -1,16 +1,59 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Keypair } from '@solana/web3.js';
 import { SplBug } from "../target/types/spl_bug";
 
 describe("spl-bug", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  
+  const payer = provider.wallet as anchor.Wallet;
+  const program = anchor.workspace.SplBug as anchor.Program<SplBug>;
 
-  const program = anchor.workspace.SplBug as Program<SplBug>;
+  const metadata = {
+    name: 'Solana Gold',
+    symbol: 'GOLDSOL',
+    uri: 'https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json',
+  };
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Create an SPL Token!", async () => {
+    // Generate new keypair to use as address for mint account.
+    const mintKeypair = new Keypair();
+
+    // SPL token default = 9 decimals
+    const transactionSignature = await program.methods
+      .createTokenMint(
+        9,
+        metadata.name,
+        metadata.symbol,
+        metadata.uri,
+      ).accounts({
+        payer: payer.publicKey,
+        mintAccount: mintKeypair.publicKey,
+      }).signers([mintKeypair])
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
+  });
+
+  it('Create an NFT!', async () => {
+    // Generate new keypair to use as address for mint account.
+    const mintKeypair = new Keypair();
+
+    // NFT default = 0 decimals
+    const transactionSignature = await program.methods
+      .createTokenMint(0, metadata.name, metadata.symbol, metadata.uri)
+      .accounts({
+        payer: payer.publicKey,
+        mintAccount: mintKeypair.publicKey,
+      })
+      .signers([mintKeypair])
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
   });
 });
