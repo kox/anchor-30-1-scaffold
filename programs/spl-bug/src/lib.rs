83b1333fc591888c4610d083ec163acf510e64ca
kox <garsanzi@gmail.com>
2024-07-10 13:27:16 +0200

    custom-dao missing auth voting

--- programs/spl-bug/src/lib.rs
+++ programs/spl-bug/src/lib.rs
@@ -6,11 +6,103 @@ declare_id!("3YUAooZywRhVz8fdsDAWMPy6xC6RNM8PSqSVwUCDz7EN");
 pub mod spl_bug {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, name: String) -> Result<()> {
+        let dao_account = &mut ctx.accounts.dao_account;
+
+        dao_account.name = name;
+        dao_account.owner = *ctx.accounts.payer.key;
+
         Ok(())
+    }
+
+    pub fn propose(ctx: Context<Propose>, proposal: String, whitelist: Vec<Pubkey>) -> Result<()> {
+        let proposal_account = &mut ctx.accounts.proposal_account;
+
+        proposal_account.proposal = proposal;
+        proposal_account.votes = 0;
+        proposal_account.dao_account = ctx.accounts.dao_account.key();
+        proposal_account.whitelist = whitelist;
+
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<VoteProposalAccount>) -> Result<()> {
+        let proposal_account = &mut ctx.accounts.proposal_account;
+        let payer = &ctx.accounts.payer;
+
+        // check if the voter is in the whitelist
+        if !proposal_account.whitelist.contains(payer.key) {
+            return Err(ErrorCode::UnauthorizedVoter.into());
+        }
+
+        proposal_account.votes += 1;
+
+        Ok(())
+
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    payer: Signer<'info>,
+
+    #[account(
+        init,
+        space = 8 + DaoAccount::INIT_SPACE,
+        payer = payer,
+    )]
+    pub dao_account: Account<'info, DaoAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Propose<'info> {
+    #[account(mut)]
+    payer: Signer<'info>,
+
+    #[account(
+        init, 
+        payer = payer,
+        space = 8 + ProposalAccount::INIT_SPACE,
+    )]
+    pub proposal_account: Account<'info, ProposalAccount>,
+
+    pub dao_account: Account<'info, DaoAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct VoteProposalAccount<'info> {
+    #[account(mut)]
+    pub proposal_account: Account<'info, ProposalAccount>,
+    
+    pub payer: Signer<'info>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct DaoAccount  {
+    #[max_len(20)]
+    pub name: String,
+    pub owner: Pubkey,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct ProposalAccount {
+    pub dao_account: Pubkey,
+    #[max_len(20)]
+    pub proposal: String,
+    pub votes: u64,
+    #[max_len(20, 32)]
+    pub whitelist: Vec<Pubkey>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You are not authorized to vote on this proposal.")]
+    UnauthorizedVoter,
+}
--- tests/spl-bug.ts
+++ tests/spl-bug.ts
@@ -4,13 +4,163 @@ import { SplBug } from "../target/types/spl_bug";
 
 describe("spl-bug", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SplBug as Program<SplBug>;
 
+  
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const daoAccount = anchor.web3.Keypair.generate();
+
+    await program.methods
+      .initialize('TestDAO')
+      .accounts({
+        daoAccount: daoAccount.publicKey,
+        payer: provider.wallet.publicKey,
+      })
+      .signers([daoAccount])
+      .rpc();
+
+    const account = await program.account.daoAccount.fetch(daoAccount.publicKey);
+    console.log('DAO Name:', account.name);
+    console.log('Owner:', account.owner.toString());
   });
+ 
+  it('Creates a proposal with whitelist and votes', async () => {
+    const daoAccount = anchor.web3.Keypair.generate();
+    const proposalAccount = anchor.web3.Keypair.generate();
+    const voter = provider.wallet.publicKey;
+    const nonVoter = anchor.web3.Keypair.generate().publicKey;
+
+    await program.methods
+      .initialize('TestDAO')
+      .accounts({
+        daoAccount: daoAccount.publicKey,
+        payer: provider.wallet.publicKey,
+      })
+      .signers([daoAccount])
+      .rpc();
+
+    const account = await program.account.daoAccount.fetch(daoAccount.publicKey);
+    console.log('DAO Name:', account.name);
+    console.log('Owner:', account.owner.toString());
+
+    await program.methods
+      .propose('Proposal Test 1', [voter])
+      .accounts({
+        payer: voter,
+        daoAccount: daoAccount.publicKey,
+        proposalAccount: proposalAccount.publicKey,
+      })
+      .signers([proposalAccount])
+      .rpc();
+      // .signer([proposalAccount])
+
+      const proposal = await program.account.proposalAccount.fetch(proposalAccount.publicKey);
+      console.log('Proposal: ', proposal.proposal);
+      console.log('Whitelist: ', proposal.whitelist);    
+  }); 
+
+  it('Votes a proposal from a whitelisted user', async () => {
+    const daoAccount = anchor.web3.Keypair.generate();
+    const proposalAccount = anchor.web3.Keypair.generate();
+    const voter = provider.wallet.publicKey;
+    const nonVoter = anchor.web3.Keypair.generate().publicKey;
+
+    await program.methods
+      .initialize('TestDAO')
+      .accounts({
+        daoAccount: daoAccount.publicKey,
+        payer: provider.wallet.publicKey,
+      })
+      .signers([daoAccount])
+      .rpc();
+
+    const account = await program.account.daoAccount.fetch(daoAccount.publicKey);
+    console.log('DAO Name:', account.name);
+    console.log('Owner:', account.owner.toString());
+
+    await program.methods
+      .propose('Proposal Test 1', [voter])
+      .accounts({
+        payer: voter,
+        daoAccount: daoAccount.publicKey,
+        proposalAccount: proposalAccount.publicKey,
+      })
+      .signers([proposalAccount])
+      .rpc();
+      // .signer([proposalAccount])
+
+      const proposal = await program.account.proposalAccount.fetch(proposalAccount.publicKey);
+      console.log('Proposal: ', proposal.proposal);
+      console.log('Whitelist: ', proposal.whitelist);
+
+    // Authorized vote
+    await program.methods
+      .vote()
+      .accounts({
+        proposalAccount: proposalAccount.publicKey,
+        payer: provider.wallet.publicKey,
+      })
+      .rpc();
+      
+      let updatedProposal = await program.account.proposalAccount.fetch(proposalAccount.publicKey);
+      console.log('Votes after authorized vote:', updatedProposal.votes);
+  }); 
+
+  it('Returns an error if an authorized user tries to vote', async () => {
+    const daoAccount = anchor.web3.Keypair.generate();
+    const proposalAccount = anchor.web3.Keypair.generate();
+    const voter = provider.wallet.publicKey;
+    const nonVoter = anchor.web3.Keypair.generate().publicKey;
+
+    await program.methods
+      .initialize('TestDAO')
+      .accounts({
+        daoAccount: daoAccount.publicKey,
+        payer: provider.wallet.publicKey,
+      })
+      .signers([daoAccount])
+      .rpc();
+
+    const account = await program.account.daoAccount.fetch(daoAccount.publicKey);
+    console.log('DAO Name:', account.name);
+    console.log('Owner:', account.owner.toString());
+
+    await program.methods
+      .propose('Proposal Test 1', [voter])
+      .accounts({
+        payer: voter,
+        daoAccount: daoAccount.publicKey,
+        proposalAccount: proposalAccount.publicKey,
+      })
+      .signers([proposalAccount])
+      .rpc();
+      // .signer([proposalAccount])
+
+      const proposal = await program.account.proposalAccount.fetch(proposalAccount.publicKey);
+      console.log('Proposal: ', proposal.proposal);
+      console.log('Whitelist: ', proposal.whitelist);
+
+    try {
+      // Authorized vote
+      await program.methods
+        .vote()
+        .accounts({
+          proposalAccount: proposalAccount.publicKey,
+          payer: nonVoter.publicKey,
+        })
+        // .signer([nonVoter])
+        .rpc();
+      
+      console.log('This message should not be seen');
+    } catch(err) {
+      console.log('Unauthorized vote error', err.toString());
+    }
+      
+      /* let updatedProposal = await program.account.proposalAccount.fetch(proposalAccount.publicKey);
+      console.log('Votes after authorized vote:', updatedProposal.votes); */
+  })
 });
